d880deb530d36b72093530859efff4cfef7293e5
Malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2020-03-31 11:16:28 -0400

    Correction des types de champs de quali

--- revlit/models.py
+++ revlit/models.py
@@ -29,7 +29,7 @@ class Quanti(models.Model):
 
 class Quali(models.Model):
     reponse_valeur = models.CharField(max_length=200)
-    reponse = models.IntegerField()
+    reponse = models.CharField(max_length=200, )
 
     def __str__(self):
         return '%s' % self.reponse
